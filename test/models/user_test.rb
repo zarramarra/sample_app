2082a95ec59288c4c2f52c8897f02d4b82cba9d5
zarramarra <bilbao.ainara@gmail.com>
2019-07-08 11:49:41 +0200

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -46,7 +46,7 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
-  
+
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
@@ -56,6 +56,11 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
 
 
 
